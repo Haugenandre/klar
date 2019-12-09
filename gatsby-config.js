649352f735554e23000fb74415f23b3b54d18a71
Andre Haugen <haugenandre@hotmail.com>
2019-12-09 14:20:35 +0100

    div

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Klar',
+    description: 'Databases are typically extremely hard to query, let alone start one from scratch or deal with the content management system (CMS). That’s where Contentful and GraphQL come in. Contentful is a CMS that serves almost like a database, especially for rich content like blog posts, languages and even trivial things like links or images. GraphQL allows you to query that content is the simplest way possible.',
+    keywords: 'react, react course, react for designers' 
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,8 +10,10 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content:
+        data.site.siteMetadata.description },
+        { name: 'keywords', content: 
+        data.site.siteMetadata.keywords },
       ]}
     />
     
@@ -32,6 +34,8 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
+        keywords
       }
     }
   }
